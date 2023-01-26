26a4f465f7a90b1c48239777ad9d69a806aa4483
DenSTR <web_life@inbox.ru>
2023-01-26 16:32:18 +0300

    работа с формой - логика 1

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,18 +1,59 @@
 import React from "react";
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      bio: '',
+      age: null,
+      isHappy: false
+    }
+
+    this.handleSetFirstName = this.handleSetFirstName.bind(this);
+    this.handleSetLastName = this.handleSetLastName.bind(this);
+    this.handleSetBio = this.handleSetBio.bind(this);
+    this.handleSetBio = this.handleSetBio.bind(this);
+    this.handleSetAge = this.handleSetAge.bind(this);
+    this.handleSetIsHappy = this.handleSetIsHappy.bind(this);
+    this.addUser = this.addUser.bind(this);
+
+  }
+
+  handleSetFirstName(e) {
+    this.setState({firstName: e.target.value})
+  }
+  handleSetLastName(e) {
+    this.setState({lastName: e.target.value})
+  }
+  handleSetBio(e) {
+    this.setState({bio: e.target.value})
+  }
+  handleSetAge(e) {
+    this.setState({age: e.target.value})
+  }
+  handleSetIsHappy(e) {
+    this.setState({isHappy: e.target.checked})
+  }
+  addUser(e) {
+    console.log('click')
+  }
+
+
   render() {
     return(
       <form className='form-component'>
-        <input placeholder='Имя'/>
-        <input placeholder='Фамилия'/>
-        <textarea placeholder='Биография'/>
-        <input placeholder='Возраст'/>
+        <input placeholder='Имя' onChange={this.handleSetFirstName}/>
+        <input placeholder='Фамилия' onChange={this.handleSetLastName} />
+        <textarea placeholder='Биография' onChange={this.handleSetBio}/>
+        <input placeholder='Возраст' onChange={this.handleSetAge} />
         <div className='field'>
           <label htmlFor='isHappy'>Состояние</label>
-          <input type='checkbox' id='isHappy'/>
+          <input type='checkbox' id='isHappy' onChange={this.handleSetIsHappy}/>
         </div>
-        <button type='button'>Добавить</button>
+        <button type='button' onClick={this.addUser}>Добавить</button>
       </form>
     )
   }
