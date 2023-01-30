62770d42bd62e47383ce19d34e996ba4d8bc40e2
DenSTR <web_life@inbox.ru>
2023-01-30 15:01:03 +0300

    всплытие состояния

--- src/components/Calculator/BoilingVerdict/index.js
+++ src/components/Calculator/BoilingVerdict/index.js
@@ -1,7 +1,5 @@
 function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>Вода закипит.</p>;
-  }
+  if (props.celsius >= 100) return <p>Вода закипит.</p>;
   return <p>Вода не закипит.</p>;
 }
 
--- src/components/Calculator/TemperatureInput/index.js
+++ src/components/Calculator/TemperatureInput/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import BoilingVerdict from "../BoilingVerdict";
 
 const scaleNames = {
   c: 'Цельсия',
@@ -10,26 +9,21 @@ class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      temperature: ''
-    };
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
-    const scale = this.props.scale;
+    const {temperature, scale} = this.props;
+
     return (
       <fieldset>
         <legend>Введите температуру в градусах {scaleNames[scale]}:</legend>
         <input
           value={temperature}
           onChange={this.handleChange} />
-        <BoilingVerdict
-          celsius={parseFloat(temperature)} />
       </fieldset>
     );
   }
